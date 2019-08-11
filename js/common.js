f928c079a2d67646bc4109b5c3213fd0c6f88429
Seiko Tomihara <sabco8822@gmail.com>
2019-08-11 22:44:26 +0900

    画像差し替え

--- js/common.js
+++ js/common.js
@@ -5,22 +5,6 @@ $('.hum_menu').on('click', function(){
   $('header').toggleClass('open');
 });
 
-
-// グローバルメニュー国際化
-$("#lng-ja").on('click', function() {
-  $("#menu-sec1").text('見出しのエリア');
-  $("#menu-sec2").text('flexのエリア');
-  $("#menu-sec3").text('センタリングエリア');
-  $("#menu-sec4").text('制作実績');
-});
-
-$("#lng-en").on('click', function() {
-  $('#menu-sec1').text('Heading');
-  $('#menu-sec2').text('Flex');
-  $('#menu-sec3').text('Centering');
-  $('#menu-sec4').text('Portfolio');
-});
-
 ////////////////////////////
 // ここからプラグイン用の記述 //
 ///////////////////////////
